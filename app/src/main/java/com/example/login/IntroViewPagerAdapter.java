7a3ca9d5ca16667e6d0fafc411b220ee8e8758fb
Diana7865 <dg587741@gmail.com>
2021-02-03 08:22:07 -0500

    cip

---
+++ app/src/main/java/com/example/login/IntroViewPagerAdapter.java
@@ -0,0 +1,67 @@
+package com.example.login;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class IntroViewPagerAdapter extends PagerAdapter{
+
+    Context mContext;
+    List<ScreenItem> mListScreen;
+
+    public IntroViewPagerAdapter(Context mContext, List<ScreenItem> mListScreen) {
+        this.mContext = mContext;
+        this.mListScreen = mListScreen;
+    }
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layoutScreen = inflater.inflate(R.layout.layout_screen, null);
+
+        ImageView ScreenImg = layoutScreen.findViewById(R.id.ScreenImg);
+        TextView intro = layoutScreen.findViewById(R.id.txtIntro);
+        TextView text = layoutScreen.findViewById(R.id.txtTex);
+
+        intro.setText(mListScreen.get(position).getIntro());
+        text.setText(mListScreen.get(position).getTex());
+        ScreenImg.setImageResource(mListScreen.get(position).getScreenImg());
+
+        container.addView(layoutScreen);
+        return  layoutScreen;
+
+
+
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return mListScreen.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+        container.removeView((View) object);
+    }
+}
+
---
+++ app/src/main/java/com/example/login/ScreenItem.java
@@ -0,0 +1,36 @@
+package com.example.login;
+
+public class ScreenItem {
+    String Intro, Tex;
+    int ScreenImg;
+
+    public ScreenItem(String intro, String tex, int screenImg) {
+        Intro = intro;
+        Tex = tex;
+        ScreenImg = screenImg;
+    }
+
+    public void setIntro(String intro) {
+        Intro = intro;
+    }
+
+    public void setTex(String tex) {
+        Tex = tex;
+    }
+
+    public void setScreenImg(int screenImg) {
+        ScreenImg = screenImg;
+    }
+
+    public String getIntro() {
+        return Intro;
+    }
+
+    public String getTex() {
+        return Tex;
+    }
+
+    public int getScreenImg() {
+        return ScreenImg;
+    }
+}
